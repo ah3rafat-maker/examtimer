aa50042d5367eedec7b78ad59e770a41a2d32798
ah3rafat-maker <ah.3rafat@gmail.com>
2026-06-08 09:57:46 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,5 +5,5 @@ window.FIREBASE_CONFIG = {
   projectId: "utastimer",
   storageBucket: "utastimer.firebasestorage.app",
   messagingSenderId: "55930134594",
-  appId: "1:55930134594:web:55aef9975d924bedafcddb"
+  appId: "1:55930134594:web:03f331bf4b4c599bafcddb"
 };
